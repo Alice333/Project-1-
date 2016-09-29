4adab838519677e27b5ff6ed7fd75ed506a2c350
Alice <alice.csy@hotmail.com>
2016-09-29 00:38:34 -0400

    ShiyunChen_PseudoCode_0929
    Homework brief ideas about the Project 1

---
+++ ShiyunChen_PseudoCode_0929.java
@@ -0,0 +1,11 @@
+//pseudo code of Project 1
+
+//set the size as 500x500 or something... looks like a rectangular
+//set the color background wine red
+//set three lines appearing from the bottom of the canvas
+//then fly to the upper screen and disapper
+//set a loop for the action above, make it can continuously running
+//make a pattern at the right bottom corner, still undeclared, maybe it's like a giant flower,
+//or mysterious circles around circles (not only circles) in a self loop
+//create the same thing in the left corner
+//only thought of this so far...
